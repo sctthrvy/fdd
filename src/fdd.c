2e905fe01ef25720639ecd66f804d4f36b34eaf2
Shane Harvey <shane6harvey@yahoo.com>
2014-12-12 00:39:11 -0500

    Added support for more file creation functions.
    Added socketpair, open, and pipe

--- src/fdd.c
+++ src/fdd.c
@@ -2,7 +2,8 @@
 #define _GNU_SOURCE
 #include "fdcommon.h"
 #include "debug.h"
-#include <sys/stat.h>   /* chmod(2) */
+#include <sys/stat.h>   /* chmod(2)     */
+#include <fcntl.h>      /* open(2)      */
 #include <signal.h>     /* sigaction(2) */
 
 /* Function prototypes */
@@ -11,6 +12,7 @@ ssize_t recvfdreq(int usock, struct fdreq* req, struct ucred *cred, struct
         sockaddr_un *src);
 ssize_t sendfdresp(int usock, struct fdresp *resp, int *fds, int numfds,
         struct sockaddr_un *src);
+int handle_fdreq(ssize_t nrecv, struct fdreq *req, int fds[2], int *numfds);
 static int initusock(void);
 static int validmsg(ssize_t nrecv, struct fdreq *req);
 static void cleanup(int signum);
@@ -46,13 +48,13 @@ static void runfdd(int usock) {
     struct fdreq req;
     struct fdresp resp;
     struct ucred cred;
-    int fdslen = 0, *fds = NULL, i;
+    int numfds, fds[2], i;
 
+    debug("FDD waiting for requests\n");
     while(1) {
         nrecv = recvfdreq(usock, &req, &cred, &src);
         if(nrecv < 0) {
-            /* Possible errors EIO, ENBUFS, and ENOMEM (all really bad) */
-            error("recvmsg failed: %s\n", strerror(errno));
+            /* Fatal error on recvmsg(2) */
             return;
         }
         /* Validate the message */
@@ -61,32 +63,19 @@ static void runfdd(int usock) {
             continue;
         }
         /* Process request */
-        if(fdslen < req.numfds) {
-            fdslen = req.numfds;
-            fds = realloc(fds, fdslen * sizeof(int));
-            if(fds == NULL) {
-                /* TODO: This is wrong, memory leak */
-                error("realloc failed: %s\n", strerror(errno));
-                return;
-            }
-        }
-        resp.errnum = 0; /* success */
-        for(i = 0; i < req.numfds; ++i) {
-            fds[i] = socket(req.fdreq_domain, req.fdreq_type, req.fdreq_protocol);
-            if(fds[i] < 0) {
-                /* TODO: close all files, free memory */
-                resp.errnum = errno;
-                req.numfds = 0; /* Kernel won't pass bad fds */
-                warn("failed to create socket: %s\n", strerror(errno));
-                break;
-            }
-        }
+        resp.errnum = handle_fdreq(nrecv, &req, fds, &numfds);
+
         /* Send reply */
-        nsent = sendfdresp(usock, &resp, fds, req.numfds, &src);
+        nsent = sendfdresp(usock, &resp, fds, numfds, &src);
         if(nsent < 0) {
             error("sendmsg failed: %s\n", strerror(errno));
         }
         /* Close the file descriptors */
+        for(i = 0; i < numfds; ++i) {
+            if(close(fds[i])) {
+                warn("Failed to close fd: %s\n", strerror(errno));
+            }
+        }
     }
 }
 
@@ -105,10 +94,10 @@ ssize_t recvfdreq(int usock, struct fdreq* req, struct ucred *cred, struct
         error("malloc failed: %s\n", strerror(errno));
         return -1;
     }
-    msg.msg_name       = src;
-    msg.msg_namelen    = sizeof(struct sockaddr_un);
     iv.iov_base        = req;
     iv.iov_len         = sizeof(struct fdreq);
+    msg.msg_name       = src;
+    msg.msg_namelen    = sizeof(struct sockaddr_un);
     msg.msg_iov        = &iv;
     msg.msg_iovlen     = 1;
     msg.msg_control    = cmsg;
@@ -166,6 +155,73 @@ ssize_t sendfdresp(int usock, struct fdresp *resp, int *fds, int numfds,
     return nsent;
 }
 
+/**
+*
+* @nrecv   Number of bytes in the received req
+* @req     Pointer to a valid fdreq struct
+* @fds     Array to store the created file descriptors
+* @numfds  Result argument, stores the number of successfully created fd's
+*
+* returns  The error number to return to the client
+*/
+int handle_fdreq(ssize_t nrecv, struct fdreq *req, int fds[2], int *numfds) {
+    int err = 0;
+
+    *numfds = 0;
+    switch(req->fdcode) {
+        case FDCODE_SOCKET:
+            fds[0] = socket(req->fdreq_domain, req->fdreq_type,
+                            req->fdreq_protocol);
+            if(fds[0] < 0) {
+                err = errno;
+                warn("failed to create socket: %s\n", strerror(errno));
+            } else {
+                *numfds = 1;
+            }
+            break;
+        case FDCODE_SOCKETPAIR:
+            err = socketpair(req->fdreq_domain, req->fdreq_type,
+                             req->fdreq_protocol, fds);
+            if(err) {
+                err = errno;
+                warn("failed to create socketpair: %s\n", strerror(errno));
+            } else {
+                *numfds = 2;
+            }
+            break;
+        case FDCODE_OPEN:
+            /* Null terminate the received pathname */
+            ((char *)req)[nrecv] = '\0';
+            fds[0] = open(req->fdreq_path, req->fdreq_flags);
+            if(fds[0] < 0) {
+                err = errno;
+                warn("failed to open %s: %s\n", req->fdreq_path,
+                     strerror(errno));
+            } else {
+                *numfds = 1;
+            }
+            break;
+        case FDCODE_PIPE:
+            err = pipe(fds);
+            if(err) {
+                err = errno;
+                warn("failed to create pipe: %s\n", strerror(errno));
+            } else {
+                *numfds = 2;
+            }
+            break;
+        case FDCODE_PIPE2:
+            /* Not implemented */
+            err = ENOSYS;
+            break;
+        default:
+            /* Invalid request */
+            err = EINVAL;
+            info("Recveived unknown fdreq code: %d\n", req->fdcode);
+    }
+    return err;
+}
+
 /*
  * Creates a UNIX datagram socket for communication with libfd.
  */
@@ -175,13 +231,13 @@ static int initusock(void) {
             .sun_family = AF_UNIX,
             .sun_path   = FDD_SOCK_PATH
     };
-
     /* Create UNIX socket */
     sock = socket(AF_UNIX, SOCK_DGRAM, 0);
     if(sock < 0) {
         error("Failed to create UNIX socket: %s\n", strerror(errno));
         exit(EXIT_FAILURE);
     }
+    debug("Created UNIX socket\n");
     /* unlink the file, in case it exists already */
     unlink(FDD_SOCK_PATH);
     /* Bind to upath */
@@ -191,6 +247,7 @@ static int initusock(void) {
         close(sock);
         exit(EXIT_FAILURE);
     }
+    debug("Binded UNIX socket to %s\n", FDD_SOCK_PATH);
     /* Enable the receiving of credentials */
     opt = 1;
     err = setsockopt(sock, SOL_SOCKET, SO_PASSCRED, &opt, sizeof(opt));
@@ -199,8 +256,10 @@ static int initusock(void) {
         close(sock);
         exit(EXIT_FAILURE);
     }
+    debug("Enabled reception of credentials.\n");
     /* chmod the file to 766 so any process can use ODR */
-    err = chmod(FDD_SOCK_PATH, S_IRUSR|S_IWUSR|S_IXUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+    err = chmod(FDD_SOCK_PATH, S_IRUSR | S_IWUSR | S_IXUSR | S_IRGRP | S_IWGRP |
+                S_IROTH | S_IWOTH);
     if(err) {
         error("chmod failed: %s\n", strerror(errno));
         close(sock);
